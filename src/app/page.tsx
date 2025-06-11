6c9dfaa23c1876a88a15b8ed17374247c3a6186e
kelviny-santos <kelviny-santos@users.noreply.github.com>
2025-06-11 20:01:42 -0300

    nova home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import { redirect } from 'next/navigation'
+import { ApresentAI } from "@/components/ApresentAI"
 
 export default function Home() {
-  redirect('/apresentai')
+  return <ApresentAI />
 }
